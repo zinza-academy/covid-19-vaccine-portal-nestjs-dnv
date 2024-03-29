5f9d12d6ffc656d14920a652f7529d60d99d33b1
NguyenDonUET <nguyendoncb@gmail.com>
2024-03-29 14:05:46 +0700

    feat: create dto for update and create user

---
+++ src/users/dto/user-create.dto.ts
@@ -0,0 +1,50 @@
+import {
+  IsBoolean,
+  IsDateString,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsNumberString,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { Gender } from '../entities/users.entity';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(50)
+  fullName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  password: string;
+
+  @IsString()
+  @MaxLength(12)
+  @IsNumberString()
+  citizenId: number;
+
+  @IsDateString()
+  dateOfBirth: string;
+
+  @IsEnum(Gender)
+  gender: string;
+
+  @IsString()
+  city: string;
+
+  @IsString()
+  district: string;
+
+  @IsString()
+  ward: string;
+
+  @IsBoolean()
+  isAdmin: boolean;
+}
