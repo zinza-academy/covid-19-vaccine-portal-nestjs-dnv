92b3e2cc29e07a1ccfaf4c5572c9ca07fbb8fb28
NguyenDonUET <nguyendoncb@gmail.com>
2024-04-26 14:50:50 +0700

    feat: Create API endpoint to retrieve all user accounts (admin only).

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,16 +6,17 @@ import {
   NotFoundException,
   Param,
   Patch,
-  Post,
 } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/user-create.dto';
 import { UpdateUserDto } from './dto/user-update.dto';
+import { UsersService } from './users.service';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from '../auth/enums/role.enum';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @Roles(Role.Admin)
   @Get()
   async getAllUsers() {
     return this.usersService.findAll();
